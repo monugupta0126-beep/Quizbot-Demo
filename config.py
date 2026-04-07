e6b9a215ac9356dfb3e4bbfd09982b18e5b4e200
monugupta0126-beep <monugupta0126@gmail.com>
2026-04-07 13:25:38 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+API_ID = 31904106
+API_HASH = "f6038b8f2567cfd6e9b4c2223255de15"
+BOT_TOKEN = "8753514994:AAGbwCwus8v7KBeNHN6tXW2cZIE7vLXXCX8"
+
+MONGO_URL = "mongodb+srv://monutech:<db_password>@monutech.hjasn6b.mongodb.net/?appName=monutech"
